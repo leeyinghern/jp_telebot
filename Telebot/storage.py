1a5d7d4b2ab58be537f134f975b3603716324e4b
yinghern <yinghernlee@temasek.com.sg>
2021-02-20 21:26:34 +0800

    tf?

--- Telebot/storage.py
+++ Telebot/storage.py
@@ -13,9 +13,8 @@ def get_current_state(user_id, game_mode):
 def set_user_state(user_id,state, game_mode):
     try:
         IN_MEMORY_STORE[user_id].update({game_mode:state})
-    except KeyError:
+    except:
         IN_MEMORY_STORE[user_id] = {game_mode:state}
-        return IN_MEMORY_STORE
 
 def del_user_state(user_id):
     try:
